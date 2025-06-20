11bf83cad2a4084c2fcb990eaa13178ee570cf3e
Kris Jenkins <krisajenkins@gmail.com>
2025-06-20 10:29:44 +0100

    Remove empty experimental.lua plugin file
    - File contained no active plugins after reorganization
    - Clean up plugin structure by removing unused category

--- lua/plugins/experimental.lua
+++
@@ -1,7 +0,0 @@
--- Plugins currently being evaluated or tested
--- This file is for experimental plugins that may be promoted to other categories
-
-return {
-    -- Currently no experimental plugins
-    -- Add plugins here that you're testing before deciding on their permanence
-}
