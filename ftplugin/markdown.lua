ada86588129a3a449d06bbf6cc87b755c8331c6a
Kris Jenkins <krisajenkins@gmail.com>
2025-09-21 08:57:35 +0100

    Better syntax highlighting of markdown comments.

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -4,6 +4,7 @@ hl_store['@markup.heading.1.markdown'] = { link = 'Identifier' }
 hl_store['@markup.heading.2.markdown'] = { link = 'Function' }
 hl_store['@markup.strong.markdown_inline'] = { link = 'Keyword' }
 hl_store['@markup.italic.markdown_inline'] = { link = 'Keyword' }
+hl_store['htmlComment'] = { link = 'Comment' }
 
 for group, hl in pairs(hl_store) do
     vim.api.nvim_set_hl(0, group, hl)
