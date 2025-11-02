5464b31c795f8567ef27c47985decaceb16a9aae
Kris Jenkins <krisajenkins@gmail.com>
2025-11-02 09:14:41 +0000

    Standardizing LSP setup with vim.lsp.enable API

--- init.lua
+++ init.lua
@@ -48,9 +48,7 @@ vim.keymap.set('n', '<Leader>lp', ':Lazy profile<CR>', { desc = '[L]azy [P]rofil
 vim.cmd('colorscheme molokai')
 -- vim.cmd('colorscheme gruvbox')
 
--- Diagnostics.
-vim.keymap.set('n', '©p', vim.diagnostic.goto_prev)
-vim.keymap.set('n', '©n', vim.diagnostic.goto_next)
+-- Diagnostics are now handled by LSP plugin mappings
 vim.keymap.set('n', '<Leader>m', ':write<CR>:make<CR>', { desc = '[M]ake' })
 
 -- Syntax highlighting customisation.
@@ -94,6 +92,7 @@ vim.api.nvim_create_autocmd('TermOpen', {
     end,
 })
 
+-- LSP servers are configured using the new vim.lsp.enable() API
 vim.lsp.enable({
     'lua_ls',
     'hls',
@@ -110,4 +109,29 @@ vim.lsp.enable({
     'purescriptls',
     'ts_ls',
     'pylsp',
+})
+
+-- LSP key mappings
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<Leader>wl', function()
+            vim.print(vim.lsp.buf.list_workspace_folders())
+        end, opts)
+        vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+    end,
 })
