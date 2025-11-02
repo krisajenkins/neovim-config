572f48e68041c86725d2cfb0ee7496356200fbc2
Kris Jenkins <krisajenkins@gmail.com>
2025-11-02 09:14:41 +0000

    Improving the Lua LSP config.

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,34 +1,6 @@
 return {
     cmd = { 'lua-language-server' },
     filetypes = { 'lua' },
-    root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
-    on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if
-            vim.loop.fs_stat(path .. '/.luarc.json')
-            or vim.loop.fs_stat(path .. '/.luarc.jsonc')
-        then
-            return
-        end
-
-        client.config.settings.Lua =
-            vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                runtime = {
-                    version = 'LuaJIT',
-                },
-                workspace = {
-                    checkThirdParty = false,
-                    library = {
-                        vim.env.VIMRUNTIME,
-                    },
-                },
-            })
-    end,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' },
-            },
-        },
-    },
+    root_markers = { '.luarc.json', '.luarc.jsonc', '.git', '.jj' },
 }
+
