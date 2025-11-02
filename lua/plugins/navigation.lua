55732a5e627cfed315045a502d09046380292f79
Kris Jenkins <krisajenkins@gmail.com>
2025-11-02 09:14:41 +0000

    Updating telescope.

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,7 +1,7 @@
 return {
     { -- Everything browser
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
+        tag = '0.1.8',
         dependencies = {
             'nvim-lua/plenary.nvim',
         },
