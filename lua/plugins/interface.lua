c51680d703a8c70d9bf81c05cd179cc26261299c
Kris Jenkins <krisajenkins@gmail.com>
2025-11-02 09:14:41 +0000

    Replacing what-key with mini.clue.

--- lua/plugins/interface.lua
+++ lua/plugins/interface.lua
@@ -21,6 +21,92 @@ return {
             -- Enable nvim-web-devicons compatibility
             miniicons.mock_nvim_web_devicons()
 
+            local miniclue = require('mini.clue')
+            miniclue.setup({
+                triggers = {
+                    -- Leader triggers
+                    { mode = 'n', keys = '<Leader>' },
+                    { mode = 'x', keys = '<Leader>' },
+
+                    -- Built-in completion
+                    { mode = 'i', keys = '<C-x>' },
+
+                    -- `g` key
+                    { mode = 'n', keys = 'g' },
+                    { mode = 'x', keys = 'g' },
+
+                    -- Marks
+                    { mode = 'n', keys = "'" },
+                    { mode = 'n', keys = '`' },
+                    { mode = 'x', keys = "'" },
+                    { mode = 'x', keys = '`' },
+
+                    -- Registers
+                    { mode = 'n', keys = '"' },
+                    { mode = 'x', keys = '"' },
+                    { mode = 'i', keys = '<C-r>' },
+                    { mode = 'c', keys = '<C-r>' },
+
+                    -- Window commands
+                    { mode = 'n', keys = '<C-w>' },
+
+                    -- `z` key
+                    { mode = 'n', keys = 'z' },
+                    { mode = 'x', keys = 'z' },
+                },
+
+                clues = {
+                    -- Enhance this by adding descriptions for <Leader> mapping groups
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.marks(),
+                    miniclue.gen_clues.registers(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
+                },
+            })
+
+            local minifiles = require('mini.files')
+            minifiles.setup({
+                windows = {
+                    preview = false,
+                    width_preview = 60,
+                },
+            })
+            vim.keymap.set('n', '-', function()
+                minifiles.open(vim.api.nvim_buf_get_name(0))
+            end, { desc = 'MiniFiles Open' })
+
+            -- Custom mappings for mini.files
+            vim.api.nvim_create_autocmd('User', {
+                pattern = 'MiniFilesBufferCreate',
+                callback = function(args)
+                    local buf_id = args.data.buf_id
+                    -- Override 'l' to close mini.files when opening a file
+                    vim.keymap.set('n', 'l', function()
+                        local entry = minifiles.get_fs_entry()
+                        if entry then
+                            if entry.fs_type == 'file' then
+                                minifiles.close()
+                                vim.cmd('edit ' .. vim.fn.fnameescape(entry.path))
+                            else
+                                -- For directories, use the default behavior (navigate into)
+                                minifiles.go_in()
+                            end
+                        end
+                    end, { buffer = buf_id, desc = 'Open file (and close)' })
+
+                    -- 'L' opens file in split
+                    vim.keymap.set('n', 'L', function()
+                        local entry = minifiles.get_fs_entry()
+                        if entry and entry.fs_type == 'file' then
+                            minifiles.close()
+                            vim.cmd('vsplit ' .. vim.fn.fnameescape(entry.path))
+                        end
+                    end, { buffer = buf_id, desc = 'Open in split' })
+                end,
+            })
+
             require('mini.bracketed').setup()
             local MiniJump2d = require('mini.jump2d')
             local single_character = MiniJump2d.builtin_opts.single_character
@@ -44,22 +130,6 @@ return {
             require('mini.notify').setup()
 
             require('mini.align').setup()
-        end,
-    },
-
-    { -- Suggest keyboard shortcuts
-        'folke/which-key.nvim',
-        event = 'VeryLazy',
-        config = function()
-            local which = require('which-key')
-            which.setup()
-            which.add({
-                { '<leader>g', group = '[G]o to' },
-                { '<leader>k', group = '[K]afka & doc[K]er' },
-                { '<leader>l', group = '[L]azy' },
-                { '<leader>x', group = 'e[X]ecutor' },
-                { '<leader>d', group = '[D]ev' },
-            })
         end,
     },
 
