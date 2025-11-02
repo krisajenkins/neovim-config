ceacc81cb652077898b09202af0b641c23191237
Kris Jenkins <krisajenkins@gmail.com>
2025-11-02 09:14:41 +0000

    Add csvview.nvim plugin for pretty CSV display
    Configures border display mode, Tab/S-Tab field navigation,
    and if/af text objects for CSV field selection.

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -157,4 +157,20 @@ return {
             },
         },
     },
+
+    {
+        'hat0uma/csvview.nvim',
+        ft = { 'csv' },
+        opts = {
+            view = {
+                display_mode = 'border',
+            },
+            keymaps = {
+                jump_next_field_end = { '<Tab>', mode = { 'n', 'v' } },
+                jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'v' } },
+                textobject_field_inner = { 'if', mode = { 'o', 'x' } },
+                textobject_field_outer = { 'af', mode = { 'o', 'x' } },
+            },
+        },
+    },
 }
