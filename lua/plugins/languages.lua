da2e43325e3495ff12ce233342737e1e8a383dea
Kris Jenkins <krisajenkins@gmail.com>
2025-11-02 09:14:41 +0000

    Adding render-markdown as a plugin.
    ...which can be toggled with `<Leader>rm`

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -39,6 +39,34 @@ return {
 
     { 'bakudankun/pico-8.vim', ft = { 'pico8' } },
 
+    { -- Render markdown in-buffer (opt-in via <Leader>rm)
+        'MeanderingProgrammer/render-markdown.nvim',
+        ft = { 'markdown' },
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        opts = {
+            enabled = false, -- Don't auto-enable; toggle with :RenderMarkdown toggle
+            render_modes = { 'n', 'c' }, -- Only render in normal/command mode, not insert
+        },
+        keys = {
+            {
+                '<Leader>rm',
+                function()
+                    local rm = require('render-markdown')
+                    rm.toggle()
+                    -- Adjust conceallevel when toggling
+                    local state = require('render-markdown.state')
+                    if state.enabled then
+                        vim.opt_local.conceallevel = 2
+                    else
+                        vim.opt_local.conceallevel = 0
+                    end
+                end,
+                ft = 'markdown',
+                desc = '[R]ender [M]arkdown toggle',
+            },
+        },
+    },
+
     {
         'toitware/ide-tools',
         ft = { 'toit' },
