19a329f189701dab2c27efcf31a19deddc101302
Kris Jenkins <krisajenkins@gmail.com>
2025-11-02 09:14:41 +0000

    Adding SQL as a treesitter language.

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -14,6 +14,7 @@ return {
                 'markdown_inline',
                 'jsonc',
                 'org',
+                'sql',
                 'toml',
                 'unison',
                 'purescript',
