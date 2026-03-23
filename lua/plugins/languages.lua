e22a32335bbbb2c81912d9ddbda2653166878764
Kris Jenkins <krisajenkins@gmail.com>
2026-03-23 19:53:02 +0000

    Improving the installation of my new suite of 'canon' languages.

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -8,21 +8,17 @@ return {
             'nvim-treesitter/nvim-treesitter',
         },
     },
+    {
+        dir = '~/Work/Tools/canon-languages/nvim',
+        ft = { 'strategy', 'landscape', 'life' },
+        event = 'VeryLazy',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    },
     { -- Project parser/watcher
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         config = function(plugin)
             -- Register custom Allium treesitter parser
-            local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-            parser_config.strategy = {
-                install_info = {
-                    url = 'https://github.com/krisajenkins/tree-sitter-strategy',
-                    files = { 'src/parser.c' },
-                    branch = 'main',
-                },
-                filetype = 'strategy',
-            }
-
             require('nvim-treesitter.configs').setup({
                 modules = {},
                 ensure_installed = {
